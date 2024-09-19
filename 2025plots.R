bdb388f5a9a1db6ad68eeb0aef2af297890c096b
rquiroga7 <rquiroga@unc.edu.ar>
2024-09-19 15:02:11 -0300

    plots2025_ipc_18_28_38

--- 2025plots.R
+++ 2025plots.R
@@ -19,10 +19,6 @@ ipc <- ipc %>% mutate(cumulative = round(cumulative / normalize_value, 4))
 ipc$fecha <- as.Date(ipc$fecha, format = "%Y-%m-%d")
 
 
-#Load ipc25_18 from file
-ipc25_18 <- read.csv("ipc_proy2025_18anual.csv")
-ipc25_18$fecha <- as.Date(ipc25_18$fecha, format = "%Y-%m-%d")
-ipc25_18 <- ipc25_18 %>% mutate(ipc_indice = round(ipc_indice / normalize_value, 4)) %>% rename(cumulative = ipc_indice)
 
 
 #Read json files into table (2017-2024)
@@ -51,19 +47,6 @@ data<-data %>%
 data$fecha <- as.Date(paste(data$impacto_presupuestario_anio, data$impacto_presupuestario_mes, "01", sep = "-"), format = "%Y-%m-%d")
 
 
-#Por Universidad subparcial_desc=="Universidad Nacional de Córdoba"
-#programa_id==26 Desarrollo de la Educacion Superior
-##actividad_id==14 Asistencia Financiera para el Funcionamiento Universitarios
-##actividad_id==12 Salarios Docentes
-##actividad_id==13 Salarios No-Docentes
-##actividad_id==15 Hospitales
-##actividad_id==11 Fundar
-##actividad_id==25 Extensión
-##actividad_id==16 CyT
-##actividad_id==24 Promoción de carreras estratégicas
-##actividad_id==23 Desarrollo de Institutos Tecnologicos de Formacion Profesional
-##actividad_id==1 - Conduccion, Gestion y Apoyo a las Politicas de Educacion Superior
-
 
 data<-data %>% 
     ungroup() %>%
@@ -71,47 +54,21 @@ data<-data %>%
   left_join(ipc, by = "fecha") %>%
   mutate(credito_devengado_real = credito_devengado/cumulative) 
 
-
-#Chequear que todo esté bien
-View(data %>% 
-filter(programa_id==26 & actividad_id == 14 & impacto_presupuestario_anio==2023) %>%
-  group_by(fecha) %>% 
-  summarise(
-    credito_vigente = sum(credito_vigente),
-    credito_devengado = sum(credito_devengado),
-    credito_devengado_real = sum(credito_devengado_real),
-    cumulative = mean(cumulative)
-  )
-)
-
-#check the number of values for credito_devengado not equal to 0, for each month 
-#View(data %>% 
-  #filter(subparcial_desc=="Universidad Nacional de Córdoba") %>%
-  #group_by(fecha) %>%
-  #summarise(n = sum(credito_devengado != 0),min = min(credito_devengado[credito_devengado != 0]),max=max(credito_devengado),sum=sum(credito_devengado))
-  #)
+colors9=c("#d4d400","#d4d400","#d4d400", "#31ffff", "#31ffff", "#31ffff", "#31ffff", "#a8009d", "#a8009d")
 
 
-#Calcular data mensual
-data_mensual <- data %>% 
-  group_by(fecha,impacto_presupuestario_anio) %>%
-  filter(fecha<=max_mes) %>%
-  summarise(credito_devengado = round(sum(credito_devengado),0), credito_devengado_real = round(sum(credito_devengado_real),0))
 
-ggplot(data_mensual, aes(x=fecha)) +
-    geom_rect(aes(xmin=as.Date("2017-03-01"), xmax=as.Date("2019-11-30"), ymin=-Inf, ymax=Inf), fill="#ffffc5") +
-    geom_rect(aes(xmin=as.Date("2019-12-01"), xmax=as.Date("2023-11-30"), ymin=-Inf, ymax=Inf), fill="#a4fcfc") +
-    geom_rect(aes(xmin=as.Date("2023-12-01"), xmax=max_mes, ymin=-Inf, ymax=Inf), fill="#ffa7f8") +
-  geom_line(aes(y=credito_devengado_real),linewidth=0.6) +
-  #geom_smooth(aes(y=credito_devengado_real), method = "loess", se = FALSE, color="red") +
-  geom_smooth(aes(y=credito_devengado_real), method = "loess", se = FALSE, color="black", span = 0.3,linewidth=2) +
-  scale_x_date(date_breaks = "1 year", date_labels = "%Y-%b", limits=as.Date(c("2017-03-01",max_mes))) +
-  labs(title = "Crédito devengado por mes",
-       x = "Fecha",
-       y = "Credito devengado") +
-    #add pale yellow background for dates up to 2019-11-30, pale blue for 2019-12-01 to 2023-11-30, and pale violet for 2023-12-01 to 2021-12-31, pale red for 2022-01-01 to 2022-12-31, pale purple for 2023-01-01 to 2024-03-01
-  theme_light() +
-  theme(legend.position = "bottom")
+#Load ipc25_18 from file
+
+ipc25_18 <- read.csv("ipc_proy2025_18anual.csv")
+ipc25_18$fecha <- as.Date(ipc25_18$fecha, format = "%Y-%m-%d")
+ipc25_18 <- ipc25_18 %>% mutate(ipc_indice = round(ipc_indice / normalize_value, 4)) %>% rename(cumulative = ipc_indice)
+ipc25_28 <- read.csv("ipc_proy2025_28anual.csv")
+ipc25_28$fecha <- as.Date(ipc25_28$fecha, format = "%Y-%m-%d")
+ipc25_28 <- ipc25_28 %>% mutate(ipc_indice = round(ipc_indice / normalize_value, 4)) %>% rename(cumulative = ipc_indice)
+ipc25_38 <- read.csv("ipc_proy2025_38anual.csv")
+ipc25_38$fecha <- as.Date(ipc25_38$fecha, format = "%Y-%m-%d")
+ipc25_38 <- ipc25_38 %>% mutate(ipc_indice = round(ipc_indice / normalize_value, 4)) %>% rename(cumulative = ipc_indice)
 
 
 data_mensual <- data %>% 
@@ -142,78 +99,6 @@ resto2 <- merge(resto, ipc25_18, by.x = "fecha", by.y = "fecha") %>%
           mutate(credito_devengado = round(if_else(credito_devengado == 0, credito_devengado_real*cumulative, credito_devengado),0))
 data_mensual_2 <- rbind(data_mensual, resto2)
 
-data_anual <- data_mensual_2 %>% 
-  group_by(impacto_presupuestario_anio) %>% 
-  summarise(credito_devengado = sum(credito_devengado), credito_devengado_real = sum(credito_devengado_real))
-#########################
-
-colors8=c("#d4d400","#d4d400","#d4d400", "#31ffff", "#31ffff", "#31ffff", "#31ffff", "#a8009d")
-colors9=c("#d4d400","#d4d400","#d4d400", "#31ffff", "#31ffff", "#31ffff", "#31ffff", "#a8009d", "#a8009d")
-
-#Plot annual data show every year in x axis. Fill columns 2017-2019 in yellow, 2020-2023 in cyan and 2024 in purple
-ggplot(data_anual, aes(x=as.factor(impacto_presupuestario_anio), y=credito_devengado_real, fill=as.factor(impacto_presupuestario_anio))) +
-  geom_bar(stat="identity") +
-  labs(title = "Universidades Nacionales: Presupuesto anual devengado",subtitle=paste0("Ajustado por inflación (IPC). En pesos de ",max_mes),
-       x = "Año",
-       y = paste0("Credito anual devengado\n(millones de $ de ",max_mes)) +
-    scale_fill_manual(values=colors8) +
-  theme_light(base_size=14) +
-    geom_text(aes(y = credito_devengado_real, label = round(credito_devengado_real, 0)), vjust = -0.5,size=5) +
-  #scale y axis to show values in millions
-  scale_y_continuous(labels = scales::comma, limits = c(NA, max(data_anual$credito_devengado_real) * 1.1)) +
-  theme(legend.position = "none", plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5))+
-  labs(caption = paste0("Se ajustó el crédito devengado en cada mes por inflación mensual, utilizando el IPC (índice de precios al consumidor).\nSe asume ajuste por IPC septiembre-diciembre 2024. Se calcula el equivalente a millones de pesos de ", max_mes, " y se anualizaron los montos.\nPor Rodrigo Quiroga. Ver https://github.com/rquiroga7/presupuesto_UNC "))
-ggsave("plots/presupuesto_anual_2017-2024.png",width = 10, height = 6, units = "in",dpi=300)
-
-#Repeat plot but with 2013 == 100
-data_anual_100 <- data_anual %>% 
-  mutate(credito_devengado_real_base100 = credito_devengado_real/credito_devengado_real[1]*100)
-
-View(data_anual_100)
-
-#Plot annual data show every year in x axis. Fill columns 2017-2019 in yellow, 2020-2023 in cyan and 2024 in purple
-ggplot(data_anual_100, aes(x=as.factor(impacto_presupuestario_anio), y=credito_devengado_real_base100, fill=as.factor(impacto_presupuestario_anio))) +
-  geom_bar(stat="identity") +
-  labs(title = "Universidades Nacionales: Presupuesto anual devengado",subtitle="Ajustado por inflación (IPC). Base 100 = 2017",
-       x = "Año",
-       y = "Credito anual devengado\n(base 100 = 2017)") +
-    scale_fill_manual(values=colors8) +
-  theme_light(base_size=14) +
-    geom_text(aes(y = credito_devengado_real_base100, label = round(credito_devengado_real_base100, 0)), vjust = -0.5,size=5) +
-  #scale y axis to show values in millions
-  scale_y_continuous(labels = scales::comma, limits = c(NA, max(data_anual_100$credito_devengado_real_base100) * 1.1)) +
-  theme(legend.position = "none", plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5))+
-  labs(caption = paste0("Se ajustó el crédito devengado en cada mes por inflación mensual, utilizando el IPC (índice de precios al consumidor).\nSe asume aumentos equivalentes al IPC para septiembre-diciembre 2024 (incluyendo aguinaldos).\nPor Rodrigo Quiroga. Ver https://github.com/rquiroga7/presupuesto_UNC "))
-ggsave("plots/presupuesto_anual_100_2017-2024.png",width = 10, height = 6, units = "in",dpi=300)
-
-#Presupuesto por número de estudiantes
-anios<-c(2017,2018,2019,2020,2021,2022,2023,2024)
-estudiantes<-c(2005152,2071270,2187292,2318255,2549789,2540854,2730754,2730754)
-
-df_estudiantes<-data.frame(anio=anios,estudiantes=estudiantes)
-data_anual_estudiantes<-merge(data_anual,df_estudiantes,by.x="impacto_presupuestario_anio",by.y="anio")
-data_anual_estudiantes<-data_anual_estudiantes %>% 
-  mutate(credito_devengado_real_por_estudiante = credito_devengado_real/estudiantes) %>%
-  mutate(credito_devengado_real_por_est_100 = credito_devengado_real_por_estudiante/credito_devengado_real_por_estudiante[1]*100)
-
-
-#Plot annual data show every year in x axis. Fill columns 2017-2019 in yellow, 2020-2023 in cyan and 2024 in purple
-ggplot(data_anual_estudiantes, aes(x=as.factor(impacto_presupuestario_anio), y=credito_devengado_real_por_est_100, fill=as.factor(impacto_presupuestario_anio))) +
-  geom_bar(stat="identity") +
-  labs(title = "Universidades Nacionales: Presupuesto anual devengado",subtitle="Ajustado por inflación y número de estudiantes. Base 100 = 2017",
-       x = "Año",
-       y = "Credito anual devengado por estudiante\n(base 100 = 2017)\n") +
-    scale_fill_manual(values=colors8) +
-  theme_light(base_size=14) +
-    geom_text(aes(y = credito_devengado_real_por_est_100, label = round(credito_devengado_real_por_est_100, 0)), vjust = -0.5,size=5) +
-  #scale y axis to show values in millions
-  scale_y_continuous(labels = scales::comma, limits = c(NA, max(data_anual_estudiantes$credito_devengado_real_por_est_100) * 1.1)) +
-  theme(legend.position = "none", plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5))+
-  labs(caption = paste0("Se ajustó el crédito devengado en cada mes por inflación mensual, utilizando el IPC (índice de precios al consumidor).\nSe asume aumentos equivalentes al IPC para septiembre-diciembre 2024 (se estima el presupuesto diciembre como noviembre*1.50).\nNúmero de estudiantes según Anuario SPU. Dato 2024 estimado como = 2023. Se utiliza base 100 = 2017.\nPor Rodrigo Quiroga. Ver https://github.com/rquiroga7/presupuesto_UNC "))
-ggsave("plots/presupuesto_anual_porest_100_2017-2024.png",width = 10, height = 6, units = "in",dpi=300)
-
-
-
 
 # Annual budget for 2025 is 3804260. The ipc_indice column is the cumulative IPC index for each month. Calculate the budget for each month of 2025
 data_2025 <- data_mensual_2 %>% 
@@ -228,21 +113,11 @@ data_2025 <- data_2025 %>%
   left_join(ipc25_18, by = "fecha")
 
 # Calculate initial credito_devengado values
-data_2025 <- data_2025 %>%
-  ungroup() %>%
-  mutate(
-    initial_credito_devengado = 3804260 * cumulative / (13 * data_2025$cumulative[1])
-  )
-
+data_2025 <- data_2025 %>%   ungroup() %>%   mutate(initial_credito_devengado = 3804260 * cumulative / (13 * data_2025$cumulative[1]))
 # Apply the 1.5 multiplier to the specified dates
-data_2025 <- data_2025 %>%
-  mutate(
-    initial_credito_devengado = ifelse(fecha %in% as.Date(c("2025-12-01", "2025-06-01")), initial_credito_devengado * 1.5, initial_credito_devengado)
-  )
-
+data_2025 <- data_2025 %>%   mutate(initial_credito_devengado = ifelse(fecha %in% as.Date(c("2025-12-01", "2025-06-01")), initial_credito_devengado * 1.5, initial_credito_devengado))
 # Calculate the sum of initial values
 sum_initial <- sum(data_2025$initial_credito_devengado)
-
 # Adjust values proportionally to ensure the total sum is 3804260
 data_2025 <- data_2025 %>%
   mutate(
@@ -254,21 +129,25 @@ data_2025 <- data_2025 %>%
 # Check the sum of credito_devengado
 sum(data_2025$credito_devengado)
 
-
+#Armo data mensual incluyendo la proyección de 2025
 data_mensual_3<-rbind(data_mensual_2,data_2025)
+#chequeo
+View(data_mensual_3)  
+#Armo datos anuales incluyendo 2025, y paso a base 100
 data_anual_2025_100 <- data_mensual_3 %>% 
   group_by(impacto_presupuestario_anio) %>% 
   summarise(credito_devengado = round(sum(credito_devengado),0), credito_devengado_real = round(sum(credito_devengado_real),0)) %>%
   mutate(credito_devengado_real_base100 = credito_devengado_real/credito_devengado_real[1]*100)
 
-View(data_mensual_3)  
 
+
+#Paso a pesos de 2025-06-01
 max_mes25 <- as.Date("2025-06-01")
 normalize_value25 <- ipc25_18 %>% filter(as.Date(fecha) == max_mes25) %>% pull(cumulative)
 #Recalculate credito_devengado_real for data_mensual_3
 data_mensual_3_25 <- data_mensual_3 %>% 
-  ungroup() %>%
-  mutate(credito_devengado_real = credito_devengado/cumulative*normalize_value25)
+  ungroup() %>% mutate(credito_devengado_real = credito_devengado/cumulative*normalize_value25)
+#Paso a datos anuales
 data_anual_2025 <- data_mensual_3_25 %>% 
   group_by(impacto_presupuestario_anio) %>% 
   summarise(credito_devengado = round(sum(credito_devengado),0), credito_devengado_real = round(sum(credito_devengado_real),0))
@@ -330,3 +209,93 @@ ggplot(data_anual_estudiantes_25, aes(x=as.factor(impacto_presupuestario_anio), 
   theme(legend.position = "none", plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5))+
   labs(caption = paste0("Se ajustó el crédito devengado en cada mes por inflación mensual, utilizando el IPC (índice de precios al consumidor).\nSe asume aumentos del presupuesto equivalentes al IPC para septiembre-diciembre 2024 (incluyendo aguinaldos). \nPara 2025 se toma el proyecto de Presupuesto 2025. Número de estudiantes según Anuario SPU (2024 y 2025 estimados como = 2023).\nPor Rodrigo Quiroga. Ver https://github.com/rquiroga7/presupuesto_UNC "))
 ggsave("plots/presupuesto_anual_porest_100_2017-2025.png",width = 10, height = 6, units = "in",dpi=300)
+
+
+
+
+#######################TEST###################
+
+process_data <- function(ipc_data, normalize_value, ipc_label) {
+  data_2025 <- data_mensual_2 %>% 
+    filter(impacto_presupuestario_anio == 2024) %>%
+    mutate(impacto_presupuestario_anio = 2025) %>%
+    mutate(fecha = as.Date(paste(impacto_presupuestario_anio, impacto_presupuestario_mes, "01", sep = "-"), format = "%Y-%m-%d"))
+  
+  data_2025 <- data_2025 %>% 
+    select(-cumulative) %>%
+    ungroup() %>%
+    group_by(fecha) %>%
+    left_join(ipc_data, by = "fecha")
+  
+  data_2025 <- data_2025 %>% 
+    ungroup() %>% 
+    mutate(initial_credito_devengado = 3804260 * cumulative / (13 * data_2025$cumulative[1])) %>%
+    mutate(initial_credito_devengado = ifelse(fecha %in% as.Date(c("2025-12-01", "2025-06-01")), initial_credito_devengado * 1.5, initial_credito_devengado))
+  
+  sum_initial <- sum(data_2025$initial_credito_devengado)
+  
+  data_2025 <- data_2025 %>%
+    mutate(
+      credito_devengado = round(initial_credito_devengado * (3804260 / sum_initial), 0)
+    ) %>%
+    mutate(credito_devengado_real = round(credito_devengado / cumulative, 0)) %>%
+    select(-ipc, -initial_credito_devengado)
+  
+  data_2025 <- data_2025 %>%
+    mutate(ipc_label = ipc_label)
+  
+  return(data_2025)
+}
+
+
+# Process data for each IPC scenario
+data_2025_18 <- process_data(ipc25_18, normalize_value25, "IPC 25-18")
+data_2025_28 <- process_data(ipc25_28, normalize_value25, "IPC 25-28")
+data_2025_38 <- process_data(ipc25_38, normalize_value25, "IPC 25-38")
+
+# Combine the data
+data_2025_combined <- bind_rows(data_2025_18, data_2025_28, data_2025_38)
+
+# Combine data_mensual_3 with the processed 2025 data
+data_mensual_3_combined <- bind_rows(data_mensual_2, data_2025_combined) %>%
+#change NA to ""
+mutate(ipc_label = ifelse(is.na(ipc_label), "IPC", ipc_label))
+
+#Annualize data
+
+#Paso a pesos de 2025-06-01
+
+
+#Recalculate credito_devengado_real for data_mensual_3_combined
+data_anual_25_combined <- data_mensual_3_combined %>% 
+  ungroup() %>%
+  mutate(credito_devengado_real = credito_devengado/cumulative) %>%
+#Paso a datos anuales
+  group_by(impacto_presupuestario_anio,ipc_label) %>% 
+  summarise(credito_devengado = round(sum(credito_devengado),0), credito_devengado_real = round(sum(credito_devengado_real),0)) 
+
+#Rebase to 2017
+data_anual_25_combined <- data_anual_25_combined %>% 
+  ungroup() %>%
+  mutate(credito_devengado_real_base100 = credito_devengado_real/credito_devengado_real[1]*100)
+
+# Generate the plot
+ggplot(data_anual_25_combined, aes(x = as.factor(impacto_presupuestario_anio), y = credito_devengado_real_base100, fill = ipc_label)) +
+  geom_bar(stat = "identity", position = position_dodge(width = 0.9)) +
+  labs(title = "Universidades Nacionales: Presupuesto anual devengado",
+       subtitle = paste0("Ajustado por inflación (IPC). En pesos de ", max_mes25, ". Inflación 2025 estimada 18,3%"),
+       x = "Año",
+       y = paste0("Credito anual devengado\n(millones de $ de ", max_mes25, ")")) +
+  scale_fill_manual(values = c("IPC"="yellow","IPC 25-18" = "red", "IPC 25-28" = "blue", "IPC 25-38" = "green")) +
+  theme_light(base_size = 14) +
+  geom_text(aes(y = credito_devengado_real_base100, label = round(credito_devengado_real_base100, 0)), 
+            position = position_dodge(width = 0.9), vjust = -0.5, size = 5) +
+  scale_y_continuous(labels = scales::comma) +
+  theme(legend.position = "bottom", 
+        plot.title = element_text(hjust = 0.5), 
+        plot.subtitle = element_text(hjust = 0.5), 
+        axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(caption = paste0("Se ajustó el crédito devengado en cada mes por inflación mensual, utilizando el IPC (índice de precios al consumidor).\nSe asume ajuste por IPC septiembre-diciembre 2024. Se calcula el equivalente a millones de pesos de ", max_mes25, " y se anualizan los montos.\nPara 2025 se toma el proyecto de presupuesto 2025, y de allí una inflación anual de 18,3%.\nPor Rodrigo Quiroga. Ver https://github.com/rquiroga7/presupuesto_UNC "))
+
+# Save the plot
+ggsave("plots/presupuesto_anual_2017-2025.png", width = 10, height = 6, units = "in", dpi = 300)
